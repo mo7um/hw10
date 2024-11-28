5df3331e64c5ebd2eeceb3268aa402365a832592
Matvey Vidyasov <kicksukoh@gmail.com>
2024-11-28 20:41:15 +0300

    Созданы функции для получения основной информации о товарах с сервера, отображения товаров в браузере, добавления новых товаров, вывода всплывающих сообщений о выполненых операциях

---
+++ script.js
@@ -0,0 +1,69 @@
+const API_URL = "https://fakestoreapi.com";
+
+async function getProducts() {
+  try {
+    const responce = await fetch(`${API_URL}/products`);
+    if (!responce.ok) throw new Error(responce.statusText);
+    const products = await responce.json();
+    displayProducts(products);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function displayProducts(products) {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = "";
+  products.forEach((product) => {
+    const productElement = document.createElement("div");
+    productElement.className = "product";
+    productElement.innerHTML = `
+      <img src="${product.image}" alt="${product.title}">
+      <h3>${product.title}</h3>
+      <p>$${product.price}</p>
+      <button onclick="deleteProduct(${product.id})">Delete</button>
+    `;
+    productList.appendChild(productElement);
+  });
+
+  showMessage("Информация о товарах успешно получена");
+}
+
+async function addProduct(event) {
+  event.preventDefault(); // Предотвращаем стандартное поведение браузера, отменяем отправку формы и обновление страницы.
+  const newProduct = {
+    // Подготавливаем описание нового продукта к преобразованию в формат json дял отправки на сервер.
+    title: document.getElementById("productTitle").value,
+    price: parseFloat(document.getElementById("productPrice").value),
+    category: document.getElementById("productCategory").value,
+    description: document.getElementById("productDescription").value,
+    image: document.getElementById("productImage").value
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/products`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }, // Сообщаем серверу что отправляемые данные имеют формат json.
+      body: JSON.stringify(newProduct)
+    });
+    if (!response.ok) throw new Error("Network response was not ok"); // Проверяем выполнение запроса, если запрос не выполнен выводим сообщение об ошибке.
+    const addedProduct = await response.json();
+    showMessage("Product added successfully");
+    getProducts();
+  } catch (error) {
+    showMessage("Error adding product: " + error.message, "error");
+  }
+}
+
+function showMessage(message, type = "success") {
+  const messageElement = document.getElementById("popUpMessage");
+  messageElement.textContent = message;
+  messageElement.className = type;
+  messageElement.style.display = "block";
+  setTimeout(() => {
+    messageElement.style.display = "none";
+  }, 3000);
+}
+
+document.getElementById("addProductForm").addEventListener("submit", addProduct);
+getProducts();
